dd558c9170380e1c72e3032ed3e741170e466f94
Kevin Shook <shookke@gmail.com>
2015-05-25 16:47:02 -0400

    Added front end UI adjustments with the addition of Bootstrap

--- app/emails.py
+++ app/emails.py
@@ -18,3 +18,17 @@ def follower_notification(followed, follower):
 								user=followed, follower=follower),
 				render_template("follower_email.html",
 								user=followed, follower=follower))
+								
+from .decorators import async
+from app import app
+
+@async
+def send_async_email(app, msg):
+	with app.app_context():
+		mail.send(msg)
+		
+def send_email(subject, sender, recipients, text_body, html_body):
+	msg = Message(subject, sender=sender, recipients=recipients)
+	msg.body = text_body
+	msg.html = html_body
+	send_async_email(app, msg)
